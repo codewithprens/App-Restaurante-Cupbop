769f6472b9fb26bad16c17108d221ca3cbfb38e1
Brian <brianalbertosuarez@gmail.com>
2022-09-21 22:52:56 -0500

    Actualizacion de rutas Login y Registro

--- cupbop.py
+++ cupbop.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
  return render_template('galeria.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/SingUp')
+def SingUp():
+    return render_template('SingUp.html')
+
+@app.route('/recoverAccount')
+def recoverAcount():
+    return render_template('recoverAccount.html')
